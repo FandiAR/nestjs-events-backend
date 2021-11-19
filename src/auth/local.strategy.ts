09b4f60db6201f5e72dbfb8b7beab1b4a465257e
Fandi A Riyanto <fandiagusriyanto@Fandis-MacBook-Pro.local>
2021-11-19 08:38:08 +0700

    Section 9: 53. Hashing password with bcrypt :sparkles:

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Strategy } from 'passport-local';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -21,7 +22,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
       this.logger.debug(`User ${username} not found!`);
       throw new UnauthorizedException();
     }
-    if (password !== user.password) {
+    if (!(await bcrypt.compare(password, user.password))) {
       this.logger.debug(`Invalid credential for user ${username}!`);
       throw new UnauthorizedException();
     }
